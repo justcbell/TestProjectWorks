9025be4c04012fd85b24e9ac88ef89c8d7d27a9e
justcbell <142145571+justcbell@users.noreply.github.com>
2023-12-27 15:28:29 +0200

    27dec

--- src/test/java/runners/Runner.java
+++ src/test/java/runners/Runner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "./src/test/resources/features",//path of the features folder
         glue = {"stepdefinitions", "hooks"},//path of the stepdefinitions folder
         dryRun = false, //true--generate the missing step definitions only. Do not run the existing step definitions
-        tags = "@excel"
+        tags = "@homework1"
 )
 public class Runner {
 }
---
+++ src/test/resources/features/grid/grid.feature
@@ -0,0 +1,3 @@
+Feature: grid_feature
+  Scenario: TC01_chrome_test
+    Given user navigates to "...." using "...." browser
